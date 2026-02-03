856f312758891b264d38416b10820463a995c286
verbitskaya-i <89703021+verbitskaya-i@users.noreply.github.com>
2026-02-03 20:48:15 +0400

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('EPAM - navigate to Services -> Explore Our Client Work -> Client Work visible', async ({ page }) => {
+  // TEST_BODY_GOES_HERE
+});
